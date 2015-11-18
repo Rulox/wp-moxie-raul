7280b392735fe100cd1241c80815c18374ef02d0
Rulox <marreror91@gmail.com>
2015-11-18 19:54:40 +0000

    Create the plugin basic structure

---
+++ wp-moxie-raul.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name:     WP Moxie Raul
+ * Plugin URI:      https://github.com/Rulox/wp-moxie-raul
+ * Version:         1.0.0
+ * Author:          Raul Marrero
+ * Description:     Basic plugin that implements a simple JSON API for a WordPress Custom Post Type (Movies) and
+ *                  a basic layout that works as a demo of the plugin, built in ReactJS. Moxie test.
+ * Date:            18/11/2015
+ */
