68786460b2dd64b4a4cfd3bc5182b0ee67f53ab6
Rulox <marreror91@gmail.com>
2015-11-18 20:52:22 +0000

    Create activator and deactivator plugin classes
    * Both clases are located under /includes folder
    * Also create a class for translation (i18n) in the same folder

---
+++ includes/class-wp-moxie-raul-deactivator.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Moxie_Raul
+ * @subpackage Wp_Moxie_Raul/includes
+ * @author     Raul Marrero
+ */
+class Wp_Moxie_Raul_Deactivator {
+    /**
+     * @since    1.0.0
+     */
+    public static function deactivate() {
+    }
+}
